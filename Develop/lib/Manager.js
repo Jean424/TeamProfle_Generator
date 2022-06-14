af5fad06d7ea976dbb1a49ebf943a4e1d455a223
Jean Lee <jeanlee.jingli@icloud.com>
2022-06-13 20:27:07 -0700

    modify manager.js

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,7 +1,7 @@
 const Empolyee = require("./Employee");
 
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
-class Manager extends Employee {
+class Manager extends Empolyee {
   constructor(id, name, email, officeNumber) {
     super(id, name, email); //setting properties from parent class
     this.officeNumber = officeNumber;
